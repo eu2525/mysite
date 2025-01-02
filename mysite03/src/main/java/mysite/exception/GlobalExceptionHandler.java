f92c5dc590050adacb91dde2994c522313ce16ba
eu2525 <eu2525@khu.ac.kr>
2025-01-02 14:32:16 +0900

    exception handler & logging

---
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package mysite.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
+	
+	@ExceptionHandler(Exception.class)
+	public String handler(Model model, Exception e) {
+		// 1. 로깅(Logging)
+		StringWriter errors = new StringWriter();
+		e.printStackTrace(new PrintWriter(errors));
+		log.error(errors.toString());
+		
+		// 2. 사과 페이지(종료)
+		model.addAttribute("errors", errors.toString());
+		return "error/exception";
+		
+	}
+	
+}
